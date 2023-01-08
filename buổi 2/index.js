f2ca183f5d186fb7f655e84775bd437d80368b54
Ngoc Nguyen <ngocnguyen@MacBook-Air-cua-Ngoc.local>
2023-01-08 11:54:33 +0700

    upload

---
+++ buổi 2/index.js
@@ -0,0 +1,52 @@
+// for (var i = 0;1 < 10;i++) {
+//     console.log("Lap duoc " + i +" lan ne ");
+// }
+// var i = 0;
+// do {
+//     console.log("i = " +1);
+//     i++;
+// } while ( i< 5);
+// var danhsachhocsinh = ["Nhu", "Ngoc"]
+// danhsachhocsinh.push("Ngoc 2")// push la phuong thuc them phan tu vao mang
+// danhsachhocsinh[1] = "Nhu"
+// for ( var i = 0; i < danhsachhocsinh.length; i++){
+//     console.log (danhsachhocsinh[i]);
+// }  
+// var animals = ["Dog", "Cat", "Chicken"];
+// //create
+// animals.push('frog');
+// //read
+// console.log(animals[1]);
+// //update
+// animals[2] = "Cows";
+// // Delete
+// animals.splice(0, 1)
+// console.log(animals);
+
+// var ngoc = {
+//     age: 16,
+//     height: 163,
+// }
+// console.log(ngoc)
+// // create;
+// ngoc.work = "student";
+  
+// //read
+// console.log(ngoc.location);
+
+// //update
+// ngoc.location = "HCM"
+
+// console.log(ngoc);
+
+var myDog = {
+    'name': 'Ngao';
+    'legs': 4;
+    'friends': ['everything!']
+};
+console.log(myDog.name);
+myDog.name = Husky;
+myDog.color = "brown"
+console.log(myDog.color);
+myDog.friends;
+console.
